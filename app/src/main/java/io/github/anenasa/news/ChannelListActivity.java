87104e365d77d001150a368b99e0b1196856575c
anenasa <anenasaa@yahoo.com>
2021-09-05 11:08:38 +0800

    Replace with lambda

--- app/src/main/java/io/github/anenasa/news/ChannelListActivity.java
+++ app/src/main/java/io/github/anenasa/news/ChannelListActivity.java
@@ -3,8 +3,6 @@ package io.github.anenasa.news;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -32,14 +30,11 @@ public class ChannelListActivity extends Activity {
         channelListView.setItemsCanFocus(true);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.mylistview, displayArray);
         channelListView.setAdapter(adapter);
-        channelListView.setOnItemClickListener(new ListView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent returnIntent = new Intent();
-                returnIntent.putExtra("channelNum", channelNumArray.get(i));
-                setResult(Activity.RESULT_OK, returnIntent);
-                finish();
-            }
+        channelListView.setOnItemClickListener((adapterView, view, i, l) -> {
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("channelNum", channelNumArray.get(i));
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
         });
     }
 }
--- app/src/main/java/io/github/anenasa/news/SettingsActivity.java
+++ app/src/main/java/io/github/anenasa/news/SettingsActivity.java
@@ -7,7 +7,6 @@ import androidx.preference.PreferenceFragmentCompat;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -86,89 +85,70 @@ public class SettingsActivity extends AppCompatActivity {
             });
 
             Preference update = findPreference("update");
-            update.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    update.setSummary("正在檢查更新");
-                    new Thread( new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                URL url = new URL("https://raw.githubusercontent.com/anenasa/androidtv-news/main/VERSION");
-                                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                                urlConnection.setRequestMethod("GET");
-                                urlConnection.setReadTimeout(10000);
-                                urlConnection.setConnectTimeout(15000);
-                                urlConnection.setDoOutput(true);
-                                urlConnection.connect();
-                                InputStream inputStream = url.openStream();
-                                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                                String version_new = reader.readLine();
-                                if(!version_new.equals(BuildConfig.VERSION_NAME.split("-")[0])){
-                                    activity.runOnUiThread(() -> {
-                                        update.setSummary("有新版本");
-                                    });
-                                    String link = "https://github.com/anenasa/androidtv-news/releases/tag/v" + version_new;
-                                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
-                                    startActivity(intent);
-                                }
-                                else{
-                                    activity.runOnUiThread(() -> {
-                                        update.setSummary("已經是最新版本");
-                                    });
-                                }
-                            } catch (IOException e) {
-                                activity.runOnUiThread(() -> {
-                                    update.setSummary("更新時發生錯誤");
-                                });
-                                Log.e(TAG, Log.getStackTraceString(e));
-                            }
+            update.setOnPreferenceClickListener(preference -> {
+                update.setSummary("正在檢查更新");
+                new Thread(() -> {
+                    try {
+                        URL url = new URL("https://raw.githubusercontent.com/anenasa/androidtv-news/main/VERSION");
+                        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                        urlConnection.setRequestMethod("GET");
+                        urlConnection.setReadTimeout(10000);
+                        urlConnection.setConnectTimeout(15000);
+                        urlConnection.setDoOutput(true);
+                        urlConnection.connect();
+                        InputStream inputStream = url.openStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                        String version_new = reader.readLine();
+                        if(!version_new.equals(BuildConfig.VERSION_NAME.split("-")[0])){
+                            activity.runOnUiThread(() -> update.setSummary("有新版本"));
+                            String link = "https://github.com/anenasa/androidtv-news/releases/tag/v" + version_new;
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                            startActivity(intent);
                         }
-                    }).start();
-                    return true;
-                }
+                        else{
+                            activity.runOnUiThread(() -> update.setSummary("已經是最新版本"));
+                        }
+                    } catch (IOException e) {
+                        activity.runOnUiThread(() -> update.setSummary("更新時發生錯誤"));
+                        Log.e(TAG, Log.getStackTraceString(e));
+                    }
+                }).start();
+                return true;
             });
 
             Preference about = findPreference("about");
-            about.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    InputStream streamGPL3 = getResources().openRawResource(R.raw.gpl3);
-                    InputStream streamApache2 = getResources().openRawResource(R.raw.apache2);
-                    BufferedReader readerGPL3 = new BufferedReader(new InputStreamReader(streamGPL3));
-                    BufferedReader readerApache2 = new BufferedReader(new InputStreamReader(streamApache2));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    stringBuilder.append(String.format("新聞直播 %s 版", BuildConfig.VERSION_NAME)).append('\n')
-                            .append("專案頁面：https://github.com/anenasa/androidtv-news").append('\n')
-                            .append("授權：GNU General Public License v3.0").append('\n')
-                            .append("函式庫：").append('\n')
-                            .append("youtubedl-android - GNU General Public License v3.0").append('\n')
-                            .append("ExoPlayer - Apache License 2.0").append('\n')
-                            .append("OkHttp - Apache License 2.0").append('\n')
-                            .append('\n');
-                    try{
-                        for (String line; (line = readerGPL3.readLine()) != null; ) {
-                            stringBuilder.append(line).append('\n');
-                        }
-                        for (String line; (line = readerApache2.readLine()) != null; ) {
-                            stringBuilder.append(line).append('\n');
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
+            about.setOnPreferenceClickListener(preference -> {
+                InputStream streamGPL3 = getResources().openRawResource(R.raw.gpl3);
+                InputStream streamApache2 = getResources().openRawResource(R.raw.apache2);
+                BufferedReader readerGPL3 = new BufferedReader(new InputStreamReader(streamGPL3));
+                BufferedReader readerApache2 = new BufferedReader(new InputStreamReader(streamApache2));
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(String.format("新聞直播 %s 版", BuildConfig.VERSION_NAME)).append('\n')
+                        .append("專案頁面：https://github.com/anenasa/androidtv-news").append('\n')
+                        .append("授權：GNU General Public License v3.0").append('\n')
+                        .append("函式庫：").append('\n')
+                        .append("youtubedl-android - GNU General Public License v3.0").append('\n')
+                        .append("ExoPlayer - Apache License 2.0").append('\n')
+                        .append("OkHttp - Apache License 2.0").append('\n')
+                        .append('\n');
+                try{
+                    for (String line; (line = readerGPL3.readLine()) != null; ) {
+                        stringBuilder.append(line).append('\n');
                     }
-
-                    AlertDialog.Builder builder = new AlertDialog.Builder(preference.getContext())
-                            .setTitle("關於")
-                            .setMessage(stringBuilder.toString())
-                            .setPositiveButton("確定", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    dialog.dismiss();
-                                }
-                            });
-                            AlertDialog alertDialog = builder.create();
-                    alertDialog.show();
-                    return true;
+                    for (String line; (line = readerApache2.readLine()) != null; ) {
+                        stringBuilder.append(line).append('\n');
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(preference.getContext())
+                        .setTitle("關於")
+                        .setMessage(stringBuilder.toString())
+                        .setPositiveButton("確定", (dialog, id) -> dialog.dismiss());
+                        AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                return true;
             });
         }
     }
