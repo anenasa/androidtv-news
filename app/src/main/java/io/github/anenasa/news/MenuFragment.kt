9288cc4ca05d6c3e04770a2abf313f5d09b8fdaf
anenasa <anenasaa@yahoo.com>
2026-03-26 21:58:24 +0800

    Migrate MenuFragment.java to kotlin

---
+++ app/src/main/java/io/github/anenasa/news/MenuFragment.kt
@@ -0,0 +1,23 @@
+package io.github.anenasa.news
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MenuFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = requireView().findViewById<Button>(R.id.channel_list)
+        button.requestFocus()
+    }
+}
