49056c4eb2f329e2cda31c6652b858d4d0246835
anenasa <anenasaa@yahoo.com>
2026-03-29 10:19:42 +0800

    Migrate MySenderFactory.java to kotlin

--- app/src/main/java/io/github/anenasa/news/MySender.kt
+++ app/src/main/java/io/github/anenasa/news/MySender.kt
@@ -1,9 +1,11 @@
 package io.github.anenasa.news
 
 import android.content.Context
+import org.acra.config.CoreConfiguration
 import org.acra.data.CrashReportData
 import org.acra.sender.ReportSender
 import org.acra.sender.ReportSenderException
+import org.acra.sender.ReportSenderFactory
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -22,5 +24,11 @@ class MySender : ReportSender {
         } catch (e: IOException) {
             throw ReportSenderException("ReportSenderException", e)
         }
+    }
+}
+
+class MySenderFactory : ReportSenderFactory {
+    override fun create(context: Context, config: CoreConfiguration): ReportSender {
+        return MySender()
     }
 }
